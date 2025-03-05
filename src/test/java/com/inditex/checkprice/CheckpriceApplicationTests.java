17850bb3a27f3e68a2ea9f6b749b00003b07918c
FACUNDO RICO <facundorico@gmail.com>
2025-03-04 21:55:36 -0300

    first commit, initial project upload

---
+++ src/test/java/com/inditex/checkprice/CheckpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.checkprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
